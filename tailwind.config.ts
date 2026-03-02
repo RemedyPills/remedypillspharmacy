70212523b716d1f0a7ec7352b9df45d3f2c0382e
Adeel Siddiqui <258519373+RemedyPills@users.noreply.github.com>
2026-03-02 06:54:13 -0700

    Add Tailwind config to fix build scan

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  darkMode: ["class"],
+  content: [
+    "./client/index.html",
+    "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./shared/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
+
+export default config;
